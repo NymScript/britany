d767ceafd761b5944a624e805e73603fd79f348d
NymScript <niyimorera28@gmail.com>
2024-10-25 14:18:18 -0500

    test - 2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,19 +6,5 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   test('Prueba simple', () {
     expect(1 + 1, 2);
-  });
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Construye el widget en el entorno del test.
-    await tester.pumpWidget(MyApp());
-
-    // Verifica que el contador comienza en 0.
-    expect(find.text('0'), findsOneWidget);
-
-    // Simula un clic en el botón de incrementar.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pumpAndSettle();
-
-    // Verifica que el contador ha incrementado a 1.
-    expect(find.text('1'), findsOneWidget);
-  });
+  });  
 }
